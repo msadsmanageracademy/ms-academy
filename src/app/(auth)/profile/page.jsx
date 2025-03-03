9e2fa70cbca27dfb7d9c5a9322b17301e2806c77
msadsmanageracademy <msadsmanageracademy@gmail.com>
2025-03-03 16:20:53 -0300

    V1.1 Fixed a non-existing route inside de app, it prevented me from deploying on Vercel.

--- src/app/(auth)/profile/page.jsx
+++ src/app/(auth)/profile/page.jsx
@@ -1,19 +1,22 @@
 "use client";
 
-import ProtectedRoute from "@/components/protectedRoute/ProtectedRoute";
-import { useSession } from "@/context/SessionContext";
+// import ProtectedRoute from "@/components/protectedRoute/ProtectedRoute";
+// import { useSession } from "@/context/SessionContext";
 
 const Profile = () => {
-  const { userSession } = useSession();
+  // const { userSession } = useSession();
 
   return (
-    <ProtectedRoute>
-      <div>
-        <div>Este es el perfil personal del usuario</div>
-        <div>Nombre: {userSession?.first_name}</div>
-        <div>Apellido: {userSession?.last_name}</div>
-      </div>
-    </ProtectedRoute>
+    <>
+      <div>Esta es la página del perfil de usuario</div>
+    </>
+    // <ProtectedRoute>
+    // <div>
+    //   <div>Este es el perfil personal del usuario</div>
+    //   <div>Nombre: {userSession?.first_name}</div>
+    //   <div>Apellido: {userSession?.last_name}</div>
+    // </div>
+    // </ProtectedRoute>
   );
 };
 
